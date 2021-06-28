53e0202ab80a46694bf073990c6edc8c318d116a
gary <garykwok70@hotmail.com>
2021-06-28 11:15:03 +0800

    hello

---
+++ app/src/main/java/com/example/gary/buttontesting/Activity2.java
@@ -0,0 +1,19 @@
+package com.example.gary.buttontesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+
+    }
+}
